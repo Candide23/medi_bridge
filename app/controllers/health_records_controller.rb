4e6fcef1d57f1af31c105e5eebd5de43ff30d293
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2025-09-02 14:12:46 -0500

    add translate and toggle_view actions

--- app/controllers/health_records_controller.rb
+++ app/controllers/health_records_controller.rb
@@ -6,7 +6,29 @@ class HealthRecordsController < ApplicationController
     @health_records = current_user.health_records.order(created_at: :desc)
   end
 
-  def show; end
+  def show
+    @current_view_language = session[:view_language] || @health_record.language
+    @translation = @health_record.translation_for(@current_view_language) if @current_view_language != @health_record.language
+    @available_translations = @health_record.translations.pluck(:language)
+  end
+
+    def translate
+    target_locale = params[:target_locale] || current_user.preferred_language || 'en'
+    
+    if @health_record.has_translation?(target_locale)
+      redirect_to @health_record, notice: "Translation already exists for #{language_name(target_locale)}"
+      return
+    end
+    
+    TranslationJob.perform_later(@health_record.id, target_locale, current_user.id)
+    redirect_to @health_record, notice: "Translation to #{language_name(target_locale)} has been started."
+  end
+
+    def toggle_view
+    session[:view_language] = params[:language] || @health_record.language
+    redirect_to @health_record
+  end
+  
 
   def new
     @health_record = current_user.health_records.new
@@ -48,5 +70,9 @@ end
 
   def health_record_params
     params.require(:health_record).permit(:record_type, :language, :document)
+  end
+
+    def language_name(code)
+    SUPPORTED_LANGUAGES.find { |_, c| c == code }&.first || code
   end
 end
