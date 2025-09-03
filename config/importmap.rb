cd1ce391a86b777e8d727da37bb35ce672266883
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2025-09-03 14:46:42 -0500

    resolve logout routing error with turbo-method delete

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,6 +7,8 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@hotwired/turbo", to: "@hotwired--turbo.js" # @8.0.13
 pin "@rails/actioncable/src", to: "@rails--actioncable--src.js" # @8.0.201
-pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.4/lib/assets/compiled/rails-ujs.js"
+pin "@rails/activestorage", to: "activestorage.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@rails/ujs", to: "@rails--ujs.js"
 
 
